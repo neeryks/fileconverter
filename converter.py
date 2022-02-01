e55f9de2ebd4eb1634ef4cf3e775af7d0fb14121
neeryks <akashdeep.singh49@gmail.com>
2022-02-01 22:02:42 +0530

    Completed For All Types Of Image Conversion

--- converter.py
+++ converter.py
@@ -1,5 +1,20 @@
 from PIL import Image
 
-gv = Image.open("001.png")
-br = gv.convert('RGB')
-br.save('io.jpg')
+class converters:
+    def __init__(self,imageinput,convertto,ext,ext1):
+        self.imageinput = imageinput
+        self.convertto = convertto
+        self.ext = ext
+        self.ext1 = ext1
+    
+    def converter(set):
+        capimage = Image.open(set.imageinput)
+        if set.ext == "png" :
+            capimage = capimage.convert('RGB')
+        capimage.save("{0}.{1}".format(set.ext1,set.convertto))
+        print('File conversion done')
+
+
+imageinput,convertto= input("Enetr the URL: "),input("Type what image format to convert to: ")
+ext,ext1= imageinput.split(".")[-1],imageinput.split(".")[0]
+converters(imageinput,convertto,ext,ext1).converter()
