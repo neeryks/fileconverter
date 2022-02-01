4f0585ef75988d60407f0c27537f1ff1574ecfcb
neeryks <akashdeep.singh49@gmail.com>
2022-02-02 00:54:01 +0530

    Added Support for Heif & Heic. Updated requirements.txt

--- converter.py
+++ converter.py
@@ -1,6 +1,8 @@
 from PIL import Image
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+import pyheif
+
 class converters:
     def __init__(self,fileinput,convertto,ext,ext1):
         self.fileinput = fileinput
@@ -9,9 +11,14 @@ class converters:
         self.ext1 = ext1
     
     def imgconverter(set):
-        capimage = Image.open(set.fileinput)
-        if set.ext == "png" :
-            capimage = capimage.convert('RGB')
+        imglist = ['jpg','jpeg','png','webp']
+        if any(imglist) == set.ext:
+            capimage = Image.open(set.fileinput)
+            if set.ext == "png" :
+                capimage = capimage.convert('RGB')
+        else :
+            heif_file = pyheif.read(set.fileinput)
+            capimage  = Image.frombytes(heif_file.mode, heif_file.size, heif_file.data,"raw",heif_file.mode,heif_file.stride,)
         capimage.save("{0}.{1}".format(set.ext1,set.convertto))
         print('File conversion done')
 
