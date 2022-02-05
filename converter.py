438cb277011bc8195b1823fbd6fefb3400498a6a
neeryks <akashdeep.singh49@gmail.com>
2022-02-05 09:10:29 +0530

    Bug Fixes & Added Support for PDF to JPEG

--- converter.py
+++ converter.py
@@ -3,31 +3,39 @@ from PIL import Image
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import pyheif
-
+import pdf2image as p2i
+import zipfile
 class converters:
     def __init__(self,fileinput,convertto,ext,ext1):
         self.fileinput = fileinput
         self.convertto = convertto
         self.ext = ext
         self.ext1 = ext1
+        print('Starting Conversion... {0} ---> {1}'.format(self.ext.upper(),self.convertto.upper()))
     
     def imgconverter(set):
-        print('Starting Conversion... {0} ---> {1}'.format(set.ext.upper(),set.convertto.upper()))
-        imglist = ['jpg','jpeg','png','webp','pdf']
+        imglist = ['jpg','jpeg','webp']
         heiccodec = ['heic','heif']
         if any(set.ext in extension for extension in imglist) == True :
             capimage = Image.open(set.fileinput)
-            if set.ext == "png" :
-                capimage = capimage.convert('RGB')
         elif any(set.ext in extension for extension in heiccodec) == True:
             heif_file = pyheif.read(set.fileinput)
-            capimage  = Image.frombytes(heif_file.mode, heif_file.size, heif_file.data,"raw",heif_file.mode,heif_file.stride,)
+            capimage  = Image.frombytes(heif_file.mode, heif_file.size, heif_file.data,"raw",heif_file.mode,heif_file.stride)
+        elif set.ext == "png":
+            capimage = Image.open(set.fileinput)
+            capimage = capimage.convert('RGB')
+        elif set.ext == "pdf":
+            capimage = p2i.convert_from_path(set.fileinput,fmt="jpeg",output_folder='media')
+            capimage
+            print('Converted PDF ---> ZIP')
+            return
+            
         else :
             print("File type not supported.")
             return
         capimage.save("{0}.{1}".format(set.ext1,set.convertto))
         print('Converted {0} ---> {1}'.format(set.ext.upper(),set.convertto.upper()))
-     
+        
 
 if __name__ == '__main__':
     fileinput,convertto= askopenfilename(),input("Enter Conversion Format: ")
