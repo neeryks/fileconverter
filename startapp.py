8ec0a82d58865e468b17803657f88ce355264505
neeryks <akashdeep.singh49@gmail.com>
2022-02-06 23:25:20 +0530

    Code Improvements

--- startapp.py
+++ startapp.py
@@ -1,6 +1,7 @@
 
 from tkinter.filedialog import askopenfilename
 from converter import converters as co
+import gc
 
 i = 0
 while i < 1:
@@ -13,6 +14,7 @@ while i < 1:
     elif any(ext in listvar for listvar in ['jpg','jpeg','webp','heic','heif']) == True:
         convertto = input("Enter Conversion Format: ").lower()
         co(fileinput,convertto,ext,ext1).imgconverter()
+    gc.collect()
 
 
 
