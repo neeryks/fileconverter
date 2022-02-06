7847cee6f724d1f2d2ccba2aa7017a60f1ff005a
neeryks <akashdeep.singh49@gmail.com>
2022-02-06 23:39:30 +0530

    Code Improvement in startapp.py

--- startapp.py
+++ startapp.py
@@ -1,10 +1,8 @@
 
 from tkinter.filedialog import askopenfilename
 from converter import converters as co
-import gc
 
-i = 0
-while i < 1:
+def chooser():
     input("Press Any key to select the file for conversion")
     fileinput = askopenfilename()   
     ext,ext1= fileinput.split(".")[-1].lower(),fileinput.split(".")[0]
@@ -14,8 +12,15 @@ while i < 1:
     elif any(ext in listvar for listvar in ['jpg','jpeg','webp','heic','heif']) == True:
         convertto = input("Enter Conversion Format: ").lower()
         co(fileinput,convertto,ext,ext1).imgconverter()
-    gc.collect()
+chooser()
 
+choose = input("Would You like to do another conversion Y/N :")
+if choose == 'Y' or choose == 'y':
+    chooser()
+elif choose == 'N' or choose == 'n':
+    print("Thankyou for using \n\n")
+    input("Waiting For Input")
+    chooser()
 
 
     
